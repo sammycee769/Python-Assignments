f2601c5e5d5700a9df4d8f542199872f2fd515de
chris <samuelcristian769@gmail.com>
2026-02-15 11:23:33 +0100

    tv added

---
+++ SK;s tasks/Television/TelevisionTest.py
@@ -0,0 +1,122 @@
+import unittest
+from Televison import Television
+
+class TelevisionTest (unittest.TestCase):
+    def setUp(self):
+       self.tv = Television()
+
+    def test_that_I_Plug_TV_It_is_Initially_Off(self):
+        self.assertFalse(self.tv.is_on())
+
+    def test_That_TV_Can_be_Turned_Off_After_Use(self):
+        self.tv.turn_on()
+        self.tv.turn_off()
+        self.assertFalse(self.tv.is_on())
+
+    def test_That_I_Turn_TV_On_I_Can_Increase_Volume(self):
+        self.tv.turn_on()
+        initial_volume = self.tv.get_volume()
+        self.tv.increase_volume()
+        self.assertEqual(self.tv.get_volume(), initial_volume + 1)
+
+    def test_That_I_Try_To_Increase_Volume_Without_Turning_TV_On_Volume_Remains_Same_And_Throws_An_Error(self):
+        initial_volume = self.tv.get_volume()
+        with self.assertRaises(ValueError):self.tv.increase_volume()
+        self.assertEqual(0, initial_volume)
+
+    def test_That_I_Try_To_Decrease_Volume_Without_Turning_TV_On_Volume_Remains_Same_And_Throws_An_Error(self):
+        initial_volume = self.tv.get_volume()
+        with self.assertRaises(ValueError):self.tv.increase_volume()
+        self.assertEqual(0, initial_volume)
+
+    def test_That_I_Turn_On_TV_I_Can_Not_Increase_Past_MAx(self):
+        self.tv.turn_on()
+        initial_volume = self.tv.get_volume()
+        self.assertEqual(0, initial_volume)
+
+        for volume in range(150):
+            self.tv.increase_volume()
+        self.assertEqual(self.tv.get_volume(), self.tv.MAX_VOLUME)
+
+    def test_That_I_Turn_On_TV_I_Can_Not_Reduce_Past_Min(self):
+        self.tv.turn_on()
+        initial_volume = self.tv.get_volume()
+        self.assertEqual(0, initial_volume)
+
+        for volume in range(100):
+            self.tv.decrease_volume()
+        self.assertEqual(self.tv.get_volume(), self.tv.MIN_VOLUME)
+
+    def test_That_I_Turn_TV_On_I_Can_Set_Channel(self):
+        self.tv.turn_on()
+        initial_channel = self.tv.get_channel()
+        self.assertEqual(1,initial_channel)
+
+        self.tv.set_channel(20)
+        self.assertEqual(20,self.tv.get_channel())
+
+    def test_That_I_Turn_On_TV_I_Try_To_Set_Channel_Above_The_Last_Channel_It_Throws_An_Error(self):
+        self.tv.turn_on()
+        initial_channel = self.tv.get_channel()
+        self.assertEqual(1,initial_channel)
+
+        with self.assertRaises(ValueError):self.tv.set_channel(101)
+        self.assertEqual(1,initial_channel)
+
+
+    def test_That_I_Turn_On_TV_I_Try_To_Set_Channel_Below_The_First_Channel_It_Throws_An_Error(self):
+        self.tv.turn_on()
+        initial_channel = self.tv.get_channel()
+        self.assertEqual(1, initial_channel)
+
+        with self.assertRaises(ValueError): self.tv.set_channel(0)
+        self.assertEqual(1,initial_channel)
+
+    def test_That_I_Try_To_Set_Channel_Without_Turning_TV_On_Throws_An_Error(self):
+        initial_channel = self.tv.get_channel()
+        self.assertEqual(1,initial_channel)
+
+        with self.assertRaises(ValueError): self.tv.set_channel(25)
+        self.assertEqual(1, initial_channel)
+
+    def test_That_I_Turn_On_TV_I_Can_Change_Channel_Forward(self):
+        self.tv.turn_on()
+        initial_channel = self.tv.get_channel()
+        self.assertEqual(1, initial_channel)
+
+        self.tv.next_channel()
+        self.assertEqual(2,self.tv.get_channel())
+
+    def test_That_I_Turn_On_TV_I_Can_Change_Channel_Backward(self):
+        self.tv.turn_on()
+        initial_channel = self.tv.get_channel()
+        self.assertEqual(1, initial_channel)
+
+        self.tv.next_channel()
+        self.tv.previous_channel()
+        self.assertEqual(1, self.tv.get_channel())
+
+    def test_That_I_Turn_On_TV_And_I_Change_Channel_Past_The_Last_Channel_It_Starts_From_The_First(self):
+        self.tv.turn_on()
+        initial_channel = self.tv.get_channel()
+        self.assertEqual(1, initial_channel)
+
+        self.tv.set_channel(100)
+        self.tv.next_channel()
+        self.assertEqual(1,self.tv.get_channel())
+
+    def test_That_I_Turn_On_TV_And_I_Change_Channel_Back_From_The_First_Channel_It_Goes_The_Last(self):
+        self.tv.turn_on()
+        initial_channel = self.tv.get_channel()
+        self.assertEqual(1, initial_channel)
+
+        self.tv.previous_channel()
+        self.assertEqual(100, self.tv.get_channel())
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ SK;s tasks/Television/Televison.py
@@ -0,0 +1,63 @@
+class Television:
+    MIN_VOLUME =0
+    MAX_VOLUME  = 100
+    LAST_CHANNEL = 100
+    FIRST_CHANNEL = 1
+
+    def __init__(self):
+        self.__is_on = False
+        self.__volume = 0
+        self.__channel = 1
+
+    def turn_on(self):
+        self.__is_on = True
+
+    def turn_off(self):
+        self.__is_on = False
+
+    def  __check_power_status(self):
+        if not self.__is_on:
+            raise ValueError("TV is Off")
+
+    def increase_volume(self):
+        self.__check_power_status()
+        if self.__volume < self.MAX_VOLUME :
+            self.__volume = self.__volume + 1
+
+    def decrease_volume(self):
+        self.__check_power_status()
+        if self.__volume > self.MIN_VOLUME:
+            self.__volume = self.__volume - 1
+
+    def __channel_validator(self,channel:int):
+        if channel < self.FIRST_CHANNEL or channel > self.LAST_CHANNEL:
+            raise ValueError("Invalid channel number")
+
+    def set_channel(self, channel:int):
+        self.__check_power_status()
+        self.__channel_validator(channel)
+        self.__channel = channel
+
+    def next_channel(self):
+        self.__check_power_status()
+        if self.__channel < self.LAST_CHANNEL:
+            self.__channel = self.__channel + 1
+        else:
+            self.__channel = self.FIRST_CHANNEL
+
+    def previous_channel(self):
+        self.__check_power_status()
+        if self.__channel > self.FIRST_CHANNEL:
+            self.__channel = self.__channel - 1
+        else:
+            self.__channel = self.LAST_CHANNEL
+
+    def get_volume(self):
+        return self.__volume
+
+    def get_channel(self):
+        return self.__channel
+
+    def is_on(self):
+        return self.__is_on
+
